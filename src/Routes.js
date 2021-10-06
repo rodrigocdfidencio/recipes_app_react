7567b6788a2322b91aad069bbeda0c79ffe17534
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-10-06 15:17:13 -0300

    App de receitas

---
+++ src/Routes.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import Details from './pages/Details';
+import Explore from './pages/Explore';
+import FoodInProgress from './pages/FoodInProgress';
+import DrinkInProgress from './pages/DrinkInProgress';
+import ExploreRecipes from './pages/ExploreRecipes';
+import ExploreIngredients from './pages/ExploreIngredients';
+import ExploreArea from './pages/ExploreArea';
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ Meals } />
+        <Route exact path="/bebidas" component={ Drinks } />
+        <Route exact path="/perfil" component={ Profile } />
+        <Route exact path="/explorar" component={ Explore } />
+        <Route exact path="/comidas/:id" component={ Details } />
+        <Route exact path="/comidas/:id/in-progress" component={ FoodInProgress } />
+        <Route exact path="/bebidas/:id" component={ Details } />
+        <Route exact path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
+        <Route exact path="/explorar/comidas" component={ ExploreRecipes } />
+        <Route exact path="/explorar/bebidas" component={ ExploreRecipes } />
+        <Route
+          path="/explorar/comidas/ingredientes"
+          component={ ExploreIngredients }
+        />
+        <Route
+          path="/explorar/bebidas/ingredientes"
+          component={ ExploreIngredients }
+        />
+        <Route
+          path="/explorar/comidas/area"
+          component={ ExploreArea }
+        />
+        <Route
+          path="/explorar/bebidas/area"
+          render={ () => <h1>Not Found</h1> }
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
